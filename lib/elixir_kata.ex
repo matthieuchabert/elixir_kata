db451b7e50dded55fcd4886c28d1a0b7856541bc
Matthieu Chabert <matthieuchabert.pro@gmail.com>
2019-11-19 14:49:10 +0100

    revisted algo

--- lib/elixir_kata.ex
+++ lib/elixir_kata.ex
@@ -1,15 +1,25 @@
 defmodule ElixirKata do
+  # -------------------------------- FIRST TRY --------------------------------
   def run(str) do
+    reverse1(str)
+  end
+
+  def reverse1(str) do
     str
     |> to_array
     |> inverse
   end
 
   def to_array(str) do
-    for x <- str, do: IO.puts(x)
+    String.codepoints(str)
   end
 
   def inverse(array) do
     array
+  end
+
+  # -------------------------------- REVISTED --------------------------------
+  def reverse2(str) do
+    String.reverse(str)
   end
 end
